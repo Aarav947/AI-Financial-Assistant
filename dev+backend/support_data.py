28a72ce752f1fc84eb3ff124ad823310904a16f3
Aarav947 <163149143+Aarav947@users.noreply.github.com>
2025-11-03 17:08:22 +0530

    Add files via upload

---
+++ dev+backend/support_data.py
@@ -0,0 +1,37 @@
+supportReferences = {
+  "SBI": {
+    "account_management": "https://sbi.co.in/web/customer-care/customer-care",
+    "fraud_risk": "https://sbi.co.in/web/yono/blog/tackling-unauthorised-transactions-together",
+    "payment_problem": "https://www.sbiepay.sbi/complainManagement/userValidation",
+    "investment_info": "https://sbifunds.com",
+    "helpline": "1800 1234, 1800 2100, 1800 11 2211, 1800 425 3800"
+  },
+  "HDFC": {
+    "account_management": "https://www.hdfcbank.com/personal/help",
+    "fraud_risk": "https://www.hdfcbank.com/personal/help/report-fraud",
+    "payment_problem": "https://www.hdfcbank.com/personal/help",
+    "investment_info": "https://www.hdfcfund.com",
+    "helpline": "1800 202 6161"
+  },
+  "ICICI": {
+    "account_management": "https://www.icicibank.com/Personal-Banking/customer-care.page",
+    "fraud_risk": "https://www.icicibank.com/customer-care/raise-customer-complaint.page",
+    "payment_problem": "https://www.icicibank.com/Support/Report-Fraud.page",
+    "investment_info": "https://www.icicipruamc.com",
+    "helpline": "1860 120 7777, 1800 108 4747"
+  },
+  "Googlepay": {
+    "account_management": "https://support.google.com/googlepay/answer/9244912?hl=en",
+    "fraud_risk": "https://www.actionfraud.org.uk/google-pay-scam-fraud/",
+    "payment_problem": "https://support.google.com/pay/india/contact/report_activity",
+    "investment_info": "https://economictimes.com/wealth/personal-finance-news/google-pay-users-can-now-use-etmoney-to-invest-in-mutual-funds-nps/articleshow/78823074.cms",
+    "helpline": "1800-419-0157"
+  },
+  "Paytm": {
+    "account_management": "https://paytm.com/help/customer-support",
+    "fraud_risk": "https://paytm.com/blog/security/",
+    "payment_problem": "https://paytm.com/help/support/contactus",
+    "investment_info": "https://paytm.com/investments/mutual-funds",
+    "helpline": "0120-4456-456"
+  }
+}
